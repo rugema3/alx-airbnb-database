bf2249a26564ee0b9615f5e0455f733c0f2222fb
rugema3 <rugema61@gmail.com>
2025-05-18 20:26:54 +0200

    implemented the following: - I Wrote a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user. - I Wrote a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,3 +4,17 @@ booking.*,
 user.*
 FROM booking
 INNER JOIN user ON booking.user_id = user.user_id;
+
+-- Select all properties and their reviews, including properties with no reviews
+SELECT 
+property.*, 
+review.*
+FROM property
+LEFT JOIN review ON property.property_id = review.property_id;
+
+-- Select all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT 
+user.*, 
+booking.*
+FROM user
+FULL OUTER JOIN booking ON user.user_id = booking.user_id;
