e6c309a79f4a287b3901cc2bf10e86cf35b41ebe
rugema3 <rugema61@gmail.com>
2025-05-18 19:51:32 +0200

    committing the readme

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,8 @@
+#Learning Objectives
+This project is designed to enhance the skills of professional developers by focusing on advanced database concepts and practices. By the end of this project, learners will:
+
+Master Advanced SQL: Write complex queries involving joins, subqueries, and aggregations for data retrieval and analysis.
+Optimize Query Performance: Analyze and refactor SQL scripts using performance tools like EXPLAIN and ANALYZE.
+Implement Indexing and Partitioning: Understand and apply indexing and table partitioning to improve database performance for large datasets.
+Monitor and Refine Performance: Continuously monitor database health and refine schemas and queries for optimal performance.
+Think Like a DBA: Learn to make data-driven decisions about schema design and optimization strategies for high-volume applications.
